ef1052775cbe2b74be63b7337ade627520bbd1f9
vickeymadhukar <vickeymadhukar026@gmail.com>
2026-06-04 22:50:33 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ dotenv.config();
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 connectDB()
   .then(() => {
